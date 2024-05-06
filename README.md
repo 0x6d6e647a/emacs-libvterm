6327432b60443c7a7a8944e5363cf3c7c14a880f
Anthony Mendez <738092+0x6d6e647a@users.noreply.github.com>
2024-05-05 17:18:06 -0700

    Improve vterm_printf example preferring modern Bash [[ tests.

--- README.md
+++ README.md
@@ -252,10 +252,10 @@ For `bash` or `zsh`, put this in your `.zshrc` or `.bashrc`
 
 ```sh
 vterm_printf() {
-    if [ -n "$TMUX" ] && ([ "${TERM%%-*}" = "tmux" ] || [ "${TERM%%-*}" = "screen" ]); then
+    if [[ ( -n "${TUMX}" ) && ( ( "${TERM%%-*}" == 'tmux' ) || ( "${TERM%%-*}" = 'screen' ) ) ]]; then
         # Tell tmux to pass the escape sequences through
         printf "\ePtmux;\e\e]%s\007\e\\" "$1"
-    elif [ "${TERM%%-*}" = "screen" ]; then
+    elif [[ "${TERM%%-*}" == 'screen' ]]; then
         # GNU screen (screen, screen-256color, screen-256color-bce)
         printf "\eP\e]%s\007\e\\" "$1"
     else
--- etc/emacs-vterm-bash.sh
+++ etc/emacs-vterm-bash.sh
@@ -4,10 +4,10 @@
 # function that helps in this task, `vterm_printf`, is defined below.
 
 function vterm_printf(){
-    if [ -n "$TMUX" ] && ([ "${TERM%%-*}" = "tmux" ] || [ "${TERM%%-*}" = "screen" ] ); then
+    if [[ ( -n "${TUMX}" ) && ( ( "${TERM%%-*}" == 'tmux' ) || ( "${TERM%%-*}" = 'screen' ) ) ]]; then
         # Tell tmux to pass the escape sequences through
         printf "\ePtmux;\e\e]%s\007\e\\" "$1"
-    elif [ "${TERM%%-*}" = "screen" ]; then
+    elif [[ "${TERM%%-*}" == 'screen' ]]; then
         # GNU screen (screen, screen-256color, screen-256color-bce)
         printf "\eP\e]%s\007\e\\" "$1"
     else
